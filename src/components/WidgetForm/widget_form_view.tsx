594763e11af90eee49e25f3b8a36990344e29e7a
Rodrigo Gomes <rodrigorbrg@gmail.com>
2022-05-08 04:20:33 -0300

    usecallback loading

--- src/components/WidgetForm/widget_form_view.tsx
+++ src/components/WidgetForm/widget_form_view.tsx
@@ -113,7 +113,7 @@ function WidgetForm() {
         </>
       )
     }
-  }, [feedBackType, setFeedBackType, feedBack, setFeedBack, screenShot, setScreenShot, done, setDone]);
+  }, [feedBackType, setFeedBackType, feedBack, setFeedBack, screenShot, setScreenShot, loading, done, setDone]);
 
   return (
     <div className=" bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto"> 
